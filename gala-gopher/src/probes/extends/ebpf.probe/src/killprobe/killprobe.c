417312702b313592ad3a6bb6791d560ae20227f5
luzhihao 00478106 <luzhihao@huawei.com>
2021-05-17 14:53:42 +0800

    apply for add syscall_kill probe

---
+++ gala-gopher/src/probes/extends/ebpf.probe/src/killprobe/killprobe.c
@@ -0,0 +1,125 @@
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <bpf/libbpf.h>
+
+#include "killprobe.skel.h"
+#include "killprobe.h"
+
+#define PROBE_NAME "kill_info"
+
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+    if (level == LIBBPF_WARN)
+        return vfprintf(stderr, format, args);
+
+    return 0;
+}
+
+
+static volatile sig_atomic_t stop;
+
+static void sig_int(int signo)
+{
+    stop = 1;
+}
+
+static void bump_memlock_rlimit(void)
+{
+    struct rlimit rlim_new = {
+        .rlim_cur = RLIM_INFINITY,
+        .rlim_max = RLIM_INFINITY,
+    };
+
+    if (setrlimit(RLIMIT_MEMLOCK, &rlim_new)) {
+        fprintf(stderr, "Failed to increase RLIMIT_MEMLOCK limit!\n");
+        exit(1);
+    }
+}
+
+
+void read_probe_data(int map_fd)
+{
+    u32 key, next_key;
+    struct val_t data = {0};
+
+    while (bpf_map_get_next_key(map_fd, &key, &next_key) != -1) {
+        if (bpf_map_lookup_elem(map_fd, &next_key, &data) != 0) {
+            continue;
+        }
+
+        fprintf(stdout,
+            "|%s|%llu|%u|%u|%s|\n",
+            PROBE_NAME,
+            data.killer_pid,
+            data.signal,
+            data.killed_pid,
+            data.comm);
+        bpf_map_delete_elem(map_fd, &next_key);
+    }
+    return;
+}
+
+void run(struct killprobe_bpf *skel)
+{
+    if (signal(SIGINT, sig_int) == SIG_ERR) {
+        fprintf(stderr, "can't set signal handler: %s\n", strerror(errno));
+        return;
+    }
+
+    while (!stop) {
+        read_probe_data(bpf_map__fd(skel->maps.kill_info_map));
+        sleep(PROBE_CYCLE_SEC);
+    }
+    return;
+}
+
+
+struct killprobe_bpf* load_and_attach_progs()
+{
+    struct killprobe_bpf *skel;
+    int err;
+    
+    /* Set up libbpf errors and debug info callback */
+    libbpf_set_print(libbpf_print_fn);
+
+    /* Bump RLIMIT_MEMLOCK to allow BPF sub-system to do anything */
+    bump_memlock_rlimit();
+
+    /* Open load and verify BPF application */
+    skel = killprobe_bpf__open_and_load();
+    if (!skel) {
+        fprintf(stderr, "Failed to open killprobe BPF skeleton\n");
+        return NULL;
+    }
+
+    /* Attach tracepoint handler */
+    err = killprobe_bpf__attach(skel);
+    if (err) {
+        fprintf(stderr, "Failed to attach killprobe BPF skeleton\n");
+        killprobe_bpf__destroy(skel);
+        return NULL;
+    }
+    
+    return skel;
+}
+
+
+int main(int argc, char **argv)
+{
+    struct killprobe_bpf *skel;
+
+    skel = load_and_attach_progs();
+    if (!skel) {
+        return -1;
+    }
+    
+    run(skel);
+
+    killprobe_bpf__destroy(skel);
+    return 0;
+}
