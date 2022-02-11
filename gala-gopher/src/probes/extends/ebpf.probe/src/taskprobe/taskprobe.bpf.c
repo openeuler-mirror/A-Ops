f30c290fa3e20e024fa8b20cc967e4fb71368234
zhaoyuxing <zhaoyuxing2@huawei.com>
2022-02-11 11:29:47 +0800

    taskprobe add filter process & add history task info to map

--- gala-gopher/src/probes/extends/ebpf.probe/src/taskprobe/taskprobe.bpf.c
+++ gala-gopher/src/probes/extends/ebpf.probe/src/taskprobe/taskprobe.bpf.c
@@ -35,26 +35,46 @@ struct bpf_map_def SEC("maps") task_exit_event = {
     .value_size = sizeof(int),
 };
 
+struct bpf_map_def SEC("maps") probe_proc_map = {
+    .type = BPF_MAP_TYPE_HASH,
+    .key_size = sizeof(struct probe_process),
+    .value_size = sizeof(int),
+    .max_entries = PROBE_PROC_MAP_ENTRY_SIZE,
+};
+
 KRAWTRACE(sched_process_fork, bpf_raw_tracepoint_args)
 {
     struct task_key parent_key = {0};
     struct task_data *parent_data;
     struct task_key child_key = {0};
     struct task_data child_data = {0};
+    struct probe_process pname = {0};
+    int flag = 0;
 
     struct task_struct* parent = (struct task_struct*)ctx->args[0];
     struct task_struct* child = (struct task_struct*)ctx->args[1];
 
-    parent_key.pid = _(parent->pid);
-    parent_data = bpf_map_lookup_elem(&task_map, &parent_key);
-    if (parent_data != (void *)0) {
-        __sync_fetch_and_add(&parent_data->fork_count, 1);
+    /* check whether process comm is in probe_process list */
+    bpf_probe_read_str(&pname.name, MAX_PROCESS_NAME_LEN * sizeof(char), (char *)child->comm);
+    char *buf = (char *)bpf_map_lookup_elem(&probe_proc_map, &pname);
+    if (buf != (void *)0) {
+        flag = *buf;
     }
+    /* if process in whitelist, update info to task_map */
+    if (flag == 1) {
+        parent_key.pid = _(parent->pid);
+        parent_data = bpf_map_lookup_elem(&task_map, &parent_key);
+        if (parent_data != (void *)0) {
+            __sync_fetch_and_add(&parent_data->fork_count, 1);
+        }
 
-    child_key.pid = _(child->pid);
-    child_data.tgid = _(child->tgid);
-    child_data.ptid = _(parent->tgid);
-    bpf_map_update_elem(&task_map, &child_key, &child_data, BPF_ANY);
+        child_key.pid = _(child->pid);
+        child_data.tgid = _(child->tgid);
+        child_data.ptid = _(parent->tgid);
+        bpf_probe_read_str(&child_data.comm, TASK_COMM_LEN * sizeof(char), (char *)child->comm);
+
+        bpf_map_update_elem(&task_map, &child_key, &child_data, BPF_ANY);
+    }
 }
 
 KRAWTRACE(sched_process_exit, bpf_raw_tracepoint_args)
--- gala-gopher/src/probes/extends/ebpf.probe/src/taskprobe/taskprobe.c
+++ gala-gopher/src/probes/extends/ebpf.probe/src/taskprobe/taskprobe.c
@@ -36,17 +36,157 @@
 #define TASK_PROBE_IO_PATH "cat /proc/%d/io"
 #define TASK_PROBE_STAT_PATH "cat /proc/%d/stat"
 #define TASK_PROBE_SMAPS_PATH "cat /proc/%d/smaps"
-
+#define PROCESS_STATUS_COMMAND \
+    "ps -eo pid,ppid,pgid,comm | grep -w \"%s\" | awk '{print $1 \"|\" $2 \"|\" $3 \"|\" $4}'"
 #define OO_NAME_TASK "task"
 
 #define TASK_PROBE_COLLECTION_PERIOD 5
 
+#define __SPLIT_NEWLINE_SYMBOL(s) \
+    do { \
+        int __len = strlen(s); \
+        if (__len > 0 && (s)[__len - 1] == '\n') { \
+            (s)[__len - 1] = 0; \
+        } \
+    } while (0) \
+
 static volatile sig_atomic_t stop = 0;
 static struct probe_params tp_params = {.period = TASK_PROBE_COLLECTION_PERIOD};
+
+static int default_probed_process_num = 6;
+static char default_probed_process_list[10][MAX_PROCESS_NAME_LEN] = {
+    "go",
+    "java",
+    "python",
+    "python3",
+    "nginx",
+    "gala-gopher"
+};
 
 static void sig_int(int signal)
 {
     stop = 1;
+}
+
+static int is_existed_process(const char *comm)
+{
+    FILE *f = NULL;
+    char cmd[COMMAND_LEN] = {0};
+    char line[LINE_BUF_LEN] = {0};
+
+    (void)snprintf(cmd, COMMAND_LEN, PROCESS_STATUS_COMMAND, comm);
+    f = popen(cmd, "r");
+    if (f == NULL) {
+        return -1;
+    }
+    if (fgets(line, LINE_BUF_LEN, f) == NULL) {
+        printf("process %s is not existed.\n", comm);
+        return -1;
+    }
+    (void)pclose(f);
+    return 0;
+}
+
+/* ps_rlt exemple:
+    ps -eo pid,ppid,pgid,comm | grep nginx | awk '{print $1 "|" $2 "|" $3 "|" $4}'
+    3144599|3144598|3144598|nginx
+    3144600|3144598|3144598|nginx
+ */
+static int parse_ps_result(const char *ps_rlt, struct task_key *k, struct task_data *d)
+{
+    int i;
+    int start = 0, j = 0;
+    char id_str[PS_TYPE_MAX][TASK_COMM_LEN] = {0};
+    int len = strlen(ps_rlt);
+
+    for (i = 0; i < len; i++) {
+        if (ps_rlt[i] == '|') {
+            (void)strncpy(id_str[j++], ps_rlt + start, i - start);
+            start = i + 1;
+        }
+    }
+    if (j != PS_TYPE_MAX - 1) {
+        return -1;
+    }
+
+    k->pid = (unsigned int)atoi(id_str[PS_TYPE_PID]);
+    d->tgid = k->pid;
+    d->ptid = (unsigned int)atoi(id_str[PS_TYPE_PPID]);
+    d->pgid = (unsigned int)atoi(id_str[PS_TYPE_PGID]);
+    (void)strncpy((char *)id_str[j], ps_rlt + start, len - start);
+    (void)strcpy((char *)d->comm, (char *)id_str[j]);
+
+    return 0;
+}
+
+static int add_process_infos(const char *comm, int map_fd)
+{
+    FILE *f = NULL;
+    char cmd[COMMAND_LEN] = {0};
+    char line[LINE_BUF_LEN] = {0};
+    struct task_key key = {0};
+    struct task_data kdata = {0};
+    int ret = 0;
+
+    (void)snprintf(cmd, COMMAND_LEN, PROCESS_STATUS_COMMAND, comm);
+    f = popen(cmd, "r");
+    if (f == NULL) {
+        return -1;
+    }
+    while (!feof(f)) {
+        if (fgets(line, LINE_BUF_LEN, f) == NULL) {
+            break;
+        }
+        __SPLIT_NEWLINE_SYMBOL(line);
+        if (parse_ps_result((char *)line, &key, &kdata) < 0) {
+            ret = -1;
+            goto out;
+        }
+        /* update task_map */
+        bpf_map_update_elem(map_fd, &key, &kdata, BPF_ANY);
+    }
+out:
+    pclose(f);
+    return ret;
+}
+
+static int update_existed_task_to_map(int p_fd, int map_fd)
+{
+    struct probe_process ckey = {0};
+    struct probe_process nkey = {0};
+    int flag;
+    int ret = -1;
+
+    while (bpf_map_get_next_key(p_fd, &ckey, &nkey) != -1) {
+        ret = bpf_map_lookup_elem(p_fd, &nkey, &flag);
+        if (ret == 0) {
+            /* check whether process existed, if it is, add process to task_map */
+            if (is_existed_process((char *)nkey.name) >= 0) {
+                /* add other info of process */
+                if (add_process_infos((char *)nkey.name, map_fd) < 0) {
+		    printf("add process info fail.\n");
+                    return -1;
+                }
+            }
+        }
+        ckey = nkey;
+    }
+
+    return 0;
+}
+
+static int update_default_probed_process_to_map(int p_fd)
+{
+    struct probe_process pname;
+    int flag = 1;
+
+    for (int i = 0; i < default_probed_process_num; i++) {
+        memset(pname.name, 0, MAX_PROCESS_NAME_LEN);
+        strcpy(pname.name, default_probed_process_list[i]);
+        /* update task_map */
+        bpf_map_update_elem(p_fd, &pname, &flag, BPF_ANY);
+    }
+    return 0;
 }
 
 static void task_probe_pull_probe_data(int map_fd)
@@ -70,6 +210,8 @@ static void task_probe_pull_probe_data(int map_fd)
 int main(int argc, char **argv)
 {
     int ret = -1;
+    int pmap_fd = -1;
+    int taskmap_fd = -1;
 
     ret = signal(SIGINT, sig_int);
     if (ret < 0) {
@@ -93,8 +235,23 @@ int main(int argc, char **argv)
     }
     printf("Exit task map pin success.\n");
 
+    pmap_fd = bpf_map__fd(skel->maps.probe_proc_map);
+    taskmap_fd = bpf_map__fd(skel->maps.task_map);
+
+    ret = update_default_probed_process_to_map(pmap_fd);
+    if (ret != 0) {
+        fprintf(stderr, "Failed to update default probe proc info to probe_process map. \n");
+        goto err;
+    }
+
+    ret = update_existed_task_to_map(pmap_fd, taskmap_fd);
+    if (ret != 0) {
+        fprintf(stderr, "Failed to update existed proc to task map. \n");
+        goto err;
+    }
+
     while (stop == 0) {
-        task_probe_pull_probe_data(bpf_map__fd(skel->maps.task_map));
+        task_probe_pull_probe_data(taskmap_fd);
         sleep(tp_params.period);
     }
 
