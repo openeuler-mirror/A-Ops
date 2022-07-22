c5b3e45312c26608420ca5103e6edfc1bfe53577
Lostwayzxc <luoshengwei@huawei.com>
2022-07-22 16:40:27 +0800

    init aops check directory

---
+++ aops-check/aops_check/url.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# ******************************************************************************
+# Copyright (c) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
+# licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN 'AS IS' BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# ******************************************************************************/
+"""
+Time:
+Author:
+Description: url set
+"""
+from aops_check.controllers import algorithm_controller
+# from aops_check.conf.constant import ADD_ALGORITHM
+
+URLS = []
+
+SPECIFIC_URLS = {
+
+}
+
+for _, value in SPECIFIC_URLS.items():
+    URLS.extend(value)
