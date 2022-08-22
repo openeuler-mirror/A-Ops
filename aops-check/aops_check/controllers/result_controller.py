87d83b6bc39572dfa403b1680aaeea60187f1551
gongzt <gong_zhengtang@163.com>
2022-08-23 03:23:55 +0800

    下载alert report 报告

--- aops-check/aops_check/controllers/result_controller.py
+++ aops-check/aops_check/controllers/result_controller.py
@@ -10,8 +10,10 @@
 # PURPOSE.
 # See the Mulan PSL v2 for more details.
 # ******************************************************************************/
-from flask import jsonify
-
+import os
+from io import StringIO
+from flask import jsonify, Response, request
+from aops_utils.restful.status import DATABASE_CONNECT_ERROR
 from aops_check.database import SESSION
 from aops_check.database.dao.result_dao import ResultDao
 from aops_check.utils.schema.result import (
@@ -113,3 +115,73 @@ class QueryDomainResultCount(BaseResponse):
                                               ResultDao(),
                                               'count_domain_check_result',
                                               SESSION))
+
+
+class DownloadAlertReport(BaseResponse):
+    """
+    Interface for download alert report
+    """
+
+    @staticmethod
+    def _str_iterator(input_str, chunk_size=1024):
+        string_io = StringIO(input_str)
+        while True:
+            chunk = string_io.read(chunk_size)
+            if not chunk:
+                break
+            yield chunk
+
+    def _file_stream(self, content, alert_id):
+        if not alert_id:
+            alert_id = "Not-Content"
+        response = Response(self._str_iterator(content))
+        response.headers["Content-Type"] = "application/octet-stream"
+        response.headers["Content-Disposition"] = f"application;file_name={alert_id}.txt"
+        return response
+
+    @staticmethod
+    def _beautify_stream_content(hosts: dict, domain: dict) -> str:
+        if not domain or not hosts.get("result"):
+            return "暂无domain和host信息"
+
+        stream_content = f"""
+        Alert ID: {domain.get('alert_id')}
+        Alert Name: {domain.get('alert_name')}
+        Domain: {domain.get('domain')}
+        Workflow ID: {domain.get('workflow_id')}
+        Workflow Name: {domain.get('workflow_name')}
+        Host Num: {domain.get('host_num')}
+        Level: {domain.get('level')}
+        Confirmed: {domain.get('confirmed')}
+        """
+        for host_id, host in hosts.get("result", dict()).items():
+            host = f"""
+            Host ID: {host_id}
+            Host Name: {host['host_name']}
+            Is Root: {host['is_root']}
+            Host Check:
+
+            """
+            check_result = ""
+            for host_check in host.get("host_check_result", list()):
+                check_result += f"""
+                Label: {host_check['label']}
+                Metric Name: {host_check['metric_name']}
+                Time: {host_check['time']}
+
+                """ + os.linesep()
+            stream_content += host + check_result
+        return stream_content
+
+    def get(self):
+        """
+        Get file stream
+        """
+        result_dao = ResultDao()
+        if not result_dao.connect(SESSION):
+            return jsonify(self.make_response(DATABASE_CONNECT_ERROR))
+
+        hosts = self.handle_request(None, result_dao, 'query_result_host')
+        domain_info = self.handle_request(None, result_dao, 'query_result_list')
+        stream_content = self._beautify_stream_content(hosts=hosts["result"], domain=domain_info)
+        return self._file_stream(content=stream_content, alert_id=request.args.get("alert_id",""))
