8cd8789944223dfbbed5b9cfe7238741c2ea110f
dowzyx <zhaoyuxing2@huawei.com>
2022-10-28 14:36:06 +0800

    delete gala-anteater & gala-gopher & gala-spider

--- gala-spider/spider/conf/__init__.py
+++
@@ -1,4 +0,0 @@
-from .config import SpiderConfig
-from .config import init_spider_config
-from .observe_meta import init_observe_meta_config
-from .observe_meta import get_entity_topo_level
--- gala-spider/spider/entity_mgt/__init__.py
+++
@@ -1,5 +0,0 @@
-from .entity_mgt import ObserveEntityCreator
-from .entity_mgt import DirectRelationCreator
-from .entity_mgt import IndirectRelationCreator
-from .models import ObserveEntity
-from .models import Relation
