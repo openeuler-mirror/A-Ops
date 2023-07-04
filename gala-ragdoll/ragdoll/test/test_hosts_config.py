ac688080f64828b4d14565380b165974ffb614a6
leigengisoftstone <leigengg@isoftstone.com>
2023-07-04 14:26:31 +0800

    新增功能 支持导入/etc/hosts溯源配置
    由于/etc/hosts这种类型的配置文件的存在，后端会根据该文件名走他相应的解析流程，解析流程包括匹配用户入参的k是否为ipv4和ipv6地址，空格后是否包含相应的v的值，最终将配置转化为json格式存入git仓库

--- gala-ragdoll/ragdoll/test/test_hosts_config.py
+++ gala-ragdoll/ragdoll/test/test_hosts_config.py
@@ -19,7 +19,7 @@ DST_CONF = '{\n' \
 
 BASE_PATH = "ragdoll.config_model."
 CONFIG_MODEL_NAME = "Config"
-conf_type = "network"
+CONF_TYPE = "network"
 PROJECT_NAME = "_config"
 
 from ragdoll.test import BaseTestCase
@@ -27,11 +27,12 @@ from ragdoll.test import BaseTestCase
 
 class TestNetworkConfig(BaseTestCase):
 
-    def import_network_config_model(self):
+    @staticmethod
+    def import_network_config_model():
         conf_model = ""
-        project_name = conf_type + PROJECT_NAME
+        project_name = CONF_TYPE + PROJECT_NAME
         project_path = BASE_PATH + project_name
-        model_name = conf_type.capitalize() + CONFIG_MODEL_NAME
+        model_name = CONF_TYPE.capitalize() + CONFIG_MODEL_NAME
 
         try:
             project = importlib.import_module(project_path)
@@ -46,7 +47,6 @@ class TestNetworkConfig(BaseTestCase):
     def test_parse_res_to_json(self):
         conf_model = self.import_network_config_model()
         conf_json_str = conf_model.parse_res_to_json(CONFIG_TEXT)
-        print("conf_json_str : {}".format(conf_json_str))
 
         conf_json = json.loads(conf_json_str)
         self.assertEqual(len(conf_json.keys()), 2)
@@ -55,15 +55,12 @@ class TestNetworkConfig(BaseTestCase):
         conf_model = self.import_network_config_model()
         src_conf = conf_model.parse_res_to_json(CONFIG_TEXT)
         res = conf_model.conf_compare(DST_CONF, src_conf)
-        print("res : {}".format(res))
         self.assertTrue(res == "SYNCHRONIZED")
 
     def test_write_conf(self):
         conf_model = self.import_network_config_model()
         conf_dict = conf_model.read_conf(DST_CONF)
-        print("read_conf : {}".format(conf_dict))
         content = conf_model.write_conf(conf_dict)
-        print("content : {}".format(content))
         conf_content = "127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4\n" \
                        "::1 localhost localhost.localdomain localhost6 localhost6.localdomain6\n"
         self.assertTrue(content == conf_content)
