9d2930a2818ca582d3a2e8866c9243e241cf7b3b
Lostwayzxc <luoshengwei@huawei.com>
2023-12-02 17:13:10 +0800

    clean master, which has been backed up to branch(20231202)

--- aops-manager/aops_manager/deploy_manager/ansible_handler/roles/fluentd/templates/fluentd_dmesg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo " " >> /etc/rsyslog.conf
-echo "# Forward messages to localhost:{{ fluentd_demsg_port }} for fluentd" >> /etc/rsyslog.conf
-echo "*.info;mail.none;authpriv.none;cron.none               @127.0.0.1:{{ fluentd_demsg_port }}" >> /etc/rsyslog.conf
-
-systemctl restart rsyslog
